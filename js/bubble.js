f480f6e064d1fa1241d7fd46bd617bd39613ee8c
Siddhesh Mane <67231450+Siddhesh25082001@users.noreply.github.com>
2021-06-16 17:44:25 +0530

    Added two files namely sorting and bubble scripts

---
+++ js/bubble.js
@@ -0,0 +1,32 @@
+// BubbleSort Asynchronous Function
+async function BubbleSort() {
+
+    // selecting all bars with class 'bar'
+    const ele = document.querySelectorAll(".bar");
+
+    for(let i = 0; i < ele.length-1; i++)
+    {
+        for(let j = 0; j < ele.length-i-1; j++)
+        {
+            ele[j].style.background = 'cyan'; // main element
+            ele[j+1].style.background = 'red'; // adjacent element
+
+            if(parseInt(ele[j].style.height) > parseInt(ele[j+1].style.height))
+            {
+                await rukomereliye(delay); // async/await function
+                swap(ele[j], ele[j+1]);  // swap function 
+            }
+            ele[j].style.background = 'orange';
+            ele[j+1].style.background = 'orange';
+        }
+        ele[ele.length-1-i].style.background = 'lightgreen'; // Last soted element becoming lightgreen
+    }
+    ele[0].style.background = 'lightgreen'; // first soted element to make lightgreen as it is the only left at last.
+}
+
+// Selecting Bubble Sort by targeting its classname
+const bubble = document.querySelector(".bubbleSort");
+bubble.addEventListener('click', async function(){
+    await BubbleSort(); // Calling BubbleSort Function
+});
+
