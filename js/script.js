fec140141c0cf24da5c67f1ea0dbb798ed0e09b5
Siddhesh Mane <67231450+Siddhesh25082001@users.noreply.github.com>
2021-06-18 13:35:26 +0530

    Final script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,13 @@
 var icon = document.getElementById("icon");
 icon.onclick = function(){
     document.body.classList.toggle("darktheme");
+
+    if(document.body.classList.contains("darktheme")){
+         icon.innerHTML = "<i class='fa fa-sun-o'></i>"
+    }
+    else{
+     icon.innerHTML = "<i class='fa fa-moon-o'></i>"
+    }
 }
 
 // Making the header, vertical navbar and contact us div hidden untill the page loads completely.
@@ -77,5 +84,3 @@ function sortname(){
    }
    
 }
-
-
